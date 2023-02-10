40c7ddac27fb902924c79df02204c33599928eed
Nick Hamnett <nick@sameoldnick.com>
2023-02-10 15:01:01 -0700

    Add project files.

---
+++ InterfaceDemoD/FileLogger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemoD
+{
+    internal class FileLogger : ILoggerDriver
+    {
+        private StreamWriter streamWriter;
+
+        public FileLogger(string fileName) {
+            this.streamWriter = File.AppendText(fileName);
+        }
+
+        public void Log(string message)
+        {
+            this.streamWriter.WriteLine(message);
+            this.streamWriter.Flush();
+
+        }
+    }
+}
---
+++ InterfaceDemoD/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InterfaceDemoD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Does something");
+
+            //StreamWriter streamWriter;
+            //streamWriter = File.AppendText("log.txt");
+
+            //streamWriter.WriteLine("Does something");
+
+            //streamWriter.Flush();
+
+            //streamWriter.WriteLine("Does something else");
+            //streamWriter.Flush();
+
+            //streamWriter.Close();
+
+            //FileLogger fileLogger = new FileLogger();
+
+            //fileLogger.Log("Does something");
+            //fileLogger.Log("Does something else");
+
+            //NSALogger nsaLogger= new NSALogger();
+
+            //nsaLogger.Log("Listening to music");
+
+            //ILoggerDriver logger = new NSALogger();
+            ILoggerDriver logger = new FileLogger("log.txt");
+
+            logger.Log("Does something");
+            logger.Log("Does something else");
+        }
+    }
+}
